aa59c7583a3da2aa36b3cad79902d17f8e6f0a2d
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-01-20 22:46:44 -0330

    Should be final push, double checking code

--- src/temperature/TemperatureAnalyzer.java
+++ src/temperature/TemperatureAnalyzer.java
@@ -1,48 +1,68 @@
 package temperature;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TemperatureAnalyzer {
 
     public static void main(String[] args) {
 
-        /* For this project I decided to not use an array list, since we are receiving a known number of inputs
-        * and the array will only exist as long as the program is running.
-        * If we wanted to add/remove temperatures later, then we would use array list */
+        /* The initial inputs will be an array, while the array for the days that are above the average will
+        * be an array list. */
 
         // Gather number of temp inputs
-        Scanner input = new Scanner(System.in);
-        System.out.println("===============================================================");
-        System.out.println("This is the temperature analysis program!");
-        System.out.println("Please enter the number of temperatures you'd like to analyze: ");
+            Scanner input = new Scanner(System.in);
+            System.out.println("===============================================================");
+            System.out.println("This is the temperature analysis program!");
+            System.out.println("Please enter the number of temperatures you'd like to analyze: ");
 
-        int count = 0;
-        count = input.nextInt();
+            int count = 0;
+            count = input.nextInt();
 
         // Create temperature array
-        double[] temperatures = new double[count];
+            double[] temperatures = new double[count];
 
         // Get temps for each day
-        for (int i = 0; i < count; i++) {
-            System.out.println("Enter temperature for day " + (i + 1) + ": ");
-            temperatures[i] = input.nextDouble();
-        }
+            for (int i = 0; i < count; i++) {
+                System.out.println("Enter temperature for day " + (i + 1) + ": ");
+                temperatures[i] = input.nextDouble();
+            }
 
         // Calculate sum of temperatures
-        double sum = 0;
-        for (double temp : temperatures) {
-            sum += temp;
-        }
+            double sum = 0;
+            for (double temp : temperatures) {
+                sum += temp;
+            }
 
-        System.out.println("The total temperature is: " + sum);
+            System.out.println("The total temperature is: " + sum);
 
         // Calculate temperature average
-        double average = sum / count;
+            double average = sum / count;
 
+            System.out.println("The average temperate is: " + average);
 
+        // Days above average
+            ArrayList<Double> aboveAverageList = new ArrayList<>();
+            for (double temp : temperatures) {
+                if (temp > average) {
+                    aboveAverageList.add(temp);
+                }
+            }
 
-        System.out.print(Arrays.toString(temperatures));
+            // temps above average
+            if (aboveAverageList.isEmpty()) {
+                System.out.println("No temperatures are above the average temperature!");
+
+            } else if (aboveAverageList.size() == 1) {
+                System.out.println("The temperature that is above the average temperature is: " + aboveAverageList);
+                System.out.println("Days above average: " + aboveAverageList.size());
+
+            } else {
+                System.out.println("The temperatures that are above the average temperature are: " + aboveAverageList);
+                System.out.println("Days above average: " + aboveAverageList.size());
+            }
+
         input.close();
     }
 }
