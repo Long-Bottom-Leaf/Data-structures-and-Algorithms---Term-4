acf97e903d2ed87e430d758a28a955451ae56094
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-01-27 18:40:43 -0330

    Initial commit, building out basic structure

---
+++ src/MovieTheatreSeating/Main.java
@@ -0,0 +1,4 @@
+package MovieTheatreSeating;
+
+public class Main {
+}
---
+++ src/MovieTheatreSeating/TheatreSeats.java
@@ -0,0 +1,8 @@
+package MovieTheatreSeating;
+
+public class TheatreSeats {
+    // Just the main program
+    public static void main(String[] args) {
+
+    }
+}
