de6ce1b95be57ee450515f7e7d93b47f4ce85b1e
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-02-17 16:21:00 -0330

    Building Animal Shelter class

--- src/AnimalShelter/AnimalShelter.java
+++ src/AnimalShelter/AnimalShelter.java
@@ -5,4 +5,22 @@ import java.util.Queue;
 
 public class AnimalShelter {
 
+    // initialize lists
+    private Queue<Dog> dogs = new LinkedList<>();
+    private Queue<Cat> cats = new LinkedList<>();
+    private int order = 0;  // initial order
+
+    // add animal
+    public void enqueue(Animal animal) {
+        animal.setOrder(order);
+        order++;
+
+        if (animal instanceof Dog) {
+            dogs.add((Dog) animal);
+
+        } else if (animal instanceof Cat) {
+            cats.add((Cat) animal);
+        }
+    }
+
 }
